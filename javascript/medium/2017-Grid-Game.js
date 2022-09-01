fc4df77ca06db90a5358be9bd4586b136b1550e6
Anand Rajendran <imadevguyanand@gmail.com>
2022-09-01 16:07:50 -0400

    Grid game

---
+++ javascript/medium/2017-Grid-Game.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ * @complexities Time O(n) | Space => O(n), where n is the number of columns
+ * @description Prefix sums
+ */
+var gridGame = function (grid) {
+  let len = grid[0].length
+  let prefixTop = grid[0].slice()
+  let prefixBottom = grid[1].slice()
+  for (let i = 1; i < len; i++) {
+    prefixTop[i] += prefixTop[i - 1]
+    prefixBottom[i] += prefixBottom[i - 1]
+  }
+  let result = Infinity
+  for (let j = 0; j < len; j++) {
+    let top = prefixTop[len - 1] - prefixTop[j]
+    let bottom = j > 0 ? prefixBottom[j - 1] : 0
+    // From the current index, find the max points that Robot 2 can attain from either the top or bottom.
+    let r2 = Math.max(top, bottom)
+    // Keep track of the lowest max points value of all the indices visited.
+    result = Math.min(result, r2)
+  }
+  return result
+}
+
+console.log(
+  gridGame([
+    [1, 3, 1, 15],
+    [1, 3, 3, 1],
+  ])
+)
