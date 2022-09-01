4e8ac88323550e363c70b447f3b4888ab892d13b
Anand Rajendran <imadevguyanand@gmail.com>
2022-09-01 09:10:27 -0400

    Fibonacci bottom up approach

--- javascript/easy/509-Fibonacci-Number.js
+++ javascript/easy/509-Fibonacci-Number.js
@@ -23,4 +23,26 @@ function fibRec(number, memo) {
   return memo[number]
 }
 
+/**
+ * @param {number} n
+ * @return {number}
+ * @complexities Time => O(n) | Space => O(n)
+ * @description Bottom up tabulation method
+ */
+var fib = function (n) {
+  // Initialize the tabulation with length + 1
+  const tabulation = new Array(n + 1).fill(0)
+
+  // Fill the table with base case values with the return data type
+  tabulation[0] = 0
+  tabulation[1] = 1
+
+  // Bottom up logic to fill the rest of the table by iterating
+  for (let i = 2; i < tabulation.length; i++) {
+    tabulation[i] = tabulation[i - 1] + tabulation[i - 2]
+  }
+
+  return tabulation[n]
+}
+
 console.log(fib(100))
