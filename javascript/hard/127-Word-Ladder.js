758a34df1769ee8d830c7eacf970c59089c58ccd
Anand Rajendran <imadevguyanand@gmail.com>
2022-09-14 12:30:44 -0400

    Word Ladder

---
+++ javascript/hard/127-Word-Ladder.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ * @complexities Time => O(n * m ^2) | Space =>  O(n * m ^2), where n is the length of the wordlist and m is the length each word
+ */
+const ladderLength = (beginWord, endWord, wordList) => {
+  if (!wordList.includes(endWord)) {
+    return 0
+  }
+  const hashTable = {}
+  wordList.push(beginWord)
+
+  for (const word of wordList) {
+    for (let i = 0; i < word.length; i++) {
+      const pattern = word.slice(0, i) + "*" + word.slice(i + 1, word.length)
+      if (!(pattern in hashTable)) {
+        hashTable[pattern] = [word]
+      } else {
+        hashTable[pattern].push(word)
+      }
+    }
+  }
+
+  let wordCount = 1
+  const queue = [beginWord]
+  const visited = [beginWord]
+  // O(n^2 * m)
+  while (queue.length) {
+    for (let x = 0; x < queue.length; x++) {
+      const word = queue.shift()
+      if (word === endWord) {
+        return wordCount
+      }
+      for (let i = 0; i < word.length; i++) {
+        const pattern = word.slice(0, i) + "*" + word.slice(i + 1, word.length)
+        for (const newWord of hashTable[pattern]) {
+          if (newWord in visited) {
+            continue
+          }
+          visited[newWord] = true
+          queue.push(newWord)
+        }
+      }
+    }
+    wordCount += 1
+  }
+  return 0
+}
+
+console.log(ladderLength("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
+console.log(ladderLength("red", "tax", ["ted", "tex", "tax", "tad", "den", "rex", "pee"]))
