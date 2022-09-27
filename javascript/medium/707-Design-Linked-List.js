3435b6e3a907a0a937d5805f3610b8edf38682be
Anand Rajendran <imadevguyanand@gmail.com>
2022-09-27 16:43:18 -0400

    Design LinkedList

--- javascript/medium/707-Design-Linked-List.js
+++ javascript/medium/707-Design-Linked-List.js
@@ -12,13 +12,6 @@ class MyLinkedList {
     this.length = 0
   }
 
-  /**
-   *
-   * @param {number} value
-   * @return {void}
-   * @complexities Time => O(1) | Space => O(1)
-   * @description If the ll is empty, create a new node and set that as head and tail. If the ll has head then we would create a new node and point its next to the current head and set the new node as the head
-   */
   addAtHead(value) {
     const node = new Node(value)
     if (this.head) {
@@ -31,13 +24,6 @@ class MyLinkedList {
     this.length++
   }
 
-  /**
-   *
-   * @param {number} index
-   * @return {number}
-   * @complexities Time => O(n) | Space => O(1)
-   * @description Loop through the ll from 0 to index to get the value of the index. If the index is not in the range meaning if its less than 0 or greater than length - 1
-   */
   get(index) {
     if (index < 0 || index > this.length - 1) {
       return -1
@@ -49,13 +35,6 @@ class MyLinkedList {
     return current.value
   }
 
-  /**
-   *
-   * @param {number} value
-   * @return {void}
-   * @complexities Time => O(n) | Space => O(1)
-   * @description Run a loop to find the next value pointing to null. Create a new node and add the new node to its next pointer and make the new node the tail
-   */
   addAtTail(value) {
     if (this.length == 0) {
       this.addAtHead(value)
@@ -74,13 +53,6 @@ class MyLinkedList {
     return
   }
 
-  /**
-   * @param {number} index
-   * @param {number} value
-   * @return {void}
-   * @complexities Time => O(n) | Space => O(1)
-   * @description Run a loop to find the index - 1 node. Create a new node and point the index - 1 next to point to new node and point the new node's next to point to current next. If the index is 0 then call the addToHead method. If the index is equal to the length then call the addToTail method
-   */
   addAtIndex(index, value) {
     if (index > this.length) {
       return
@@ -112,12 +84,6 @@ class MyLinkedList {
     return
   }
 
-  /**
-   * @param {number} index
-   * @return {void}
-   * @complexities Time => O(n) | Space => O(1)
-   * @description Run a loop to find the index - 1 node. Set the current.next = current.next.next
-   */
   deleteAtIndex(index) {
     if (this.length === 0 || index > this.length - 1) {
       return
