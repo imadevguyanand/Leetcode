67ef9693b94c8753c74d472be27557765685df8d
Anand Rajendran <imadevguyanand@gmail.com>
2022-09-28 15:56:09 -0400

    LinkedList cycle

---
+++ javascript/easy/141-Linked-List-Cycle.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ * @complexities Time => O(n) | Space => O(1), where n is the number of nodes
+ */
+var hasCycle = function (head) {
+  let fast = head
+  let slow = head
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+    if (slow === fast) {
+      return true
+    }
+  }
+  return false
+}
